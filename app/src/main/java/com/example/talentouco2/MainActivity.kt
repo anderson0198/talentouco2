b3e082bad06f4c197b74bd0da3925b59a6ecce6a
Atzuke <40073324+Atzuke@users.noreply.github.com>
2023-10-11 16:49:11 -0500

    Se implementa el logging con google, no obstante tiene la versión "deprecated" de un metodo y no funciona por esto.

--- app/src/main/java/com/example/talentouco2/MainActivity.kt
+++ app/src/main/java/com/example/talentouco2/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.talentouco2
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -9,18 +10,32 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.NonNull
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.talentouco2.adapter.TalentoAdapter
 import com.example.talentouco2.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class MainActivity : AppCompatActivity() {
 
     //private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var client: GoogleSignInClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -45,6 +60,17 @@ class MainActivity : AppCompatActivity() {
         bundle.putString("message", "Integración de Firebase Completa")
         analytics.logEvent("InitScreen", bundle)
 
+        //Auth con google
+        val options = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("1089080056448-iqbufi63ug07q77b7n6len6opgfgv8b5.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+
+        client = GoogleSignIn.getClient(this, options)
+        binding.LogGoogle.setOnClickListener {
+            val signInIntent = client.signInIntent
+            startActivityForResult(signInIntent, 1234)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -64,10 +90,32 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    /*override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }*/
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 1234){
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account: GoogleSignInAccount = task.getResult(ApiException::class.java)
+                val credential: AuthCredential = GoogleAuthProvider.getCredential(account.idToken,null)
+                FirebaseAuth.getInstance().signInWithCredential(credential)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            // La autenticación fue exitosa
+                            val intent = Intent(applicationContext, RecyclerActivity::class.java)
+                            startActivity(intent)
+                            // Realiza acciones adicionales
+                        } else {
+                            // La autenticación falló
+
+                            Toast.makeText(applicationContext, task.exception?.message ?: "", Toast.LENGTH_SHORT).show()
+                            val intent = Intent(applicationContext, RecyclerActivity::class.java)
+                            // Manejar la excepción
+                        }
+                    }
+            }catch(e: ApiException){
+                e.printStackTrace()
+            }
+        }
+    }
 
 }
